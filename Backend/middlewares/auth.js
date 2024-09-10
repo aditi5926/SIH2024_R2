7767823573a77632b0103b4d4e56c37f1afbbb5d
sach-in2399 <awmsachin431@gmail.com>
2024-09-10 13:21:46 +0530

    change in login.js

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -21,7 +21,7 @@ exports.auth = async(req,res,next)=>{
         // verify token
         try{
             const decode = jwt.verify(token , process.env.JWT_SECRET);
-            console.log(decode);
+            console.log("This is decode",decode);
             req.user = decode;
         }
         catch(error){
